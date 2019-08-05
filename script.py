636e4e41b6146e4d851963ed7ac9c0eb28663b3b
AAbhishekReddy <annemabhishekreddy@gmail.com>
2019-08-06 00:14:06 +0530

    Intial commit

---
+++ script.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import numpy as np
+
+etf = pd.read_csv("/home/abhishek/Desktop/major/mutual/ETFs.csv")
+mut = pd.read_csv("/home/abhishek/Desktop/major/mutual/Mutual Funds.csv")
+
+mut.head()
